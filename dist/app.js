97183e1700c3cb107b848a204daf0c88c2180e5d
Felipe <felipeventurino@hotmail.com>
2019-07-27 14:15:28 -0300

    adicionando canvas 1

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,20 @@
 document.onreadystatechange = function () {
   if (document.readyState === 'complete') {
     const mathCaptcha = new MathCaptcha({
-        selector: '[name=form]'
+      selector: '[name=form]',
+      eventType: 'blur'
     })
     document.querySelector('[name=form]').addEventListener('submit', function (e) {
       e.preventDefault()
-      alert('form submitted!')
-      console.log('form submitted!')
-      this.reset()
+      document.querySelector('body').style='cursor: wait;'
+      var self = this
+      setTimeout(function() {
+        alert('form submitted!')
+        mathCaptcha.finish()
+        document.querySelector('body').style='cursor: auto;'
+        self.reset()
+        console.log('form submitted!')
+      }, 5000)
       return false
     })
   }
